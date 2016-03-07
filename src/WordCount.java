70ec7db891665333c340804e3a8e9ed1b1f49db9
bhavintandel <bhautandel@yahoo.com.com>
2016-03-07 13:45:51 +0000

    using Buffered reader

--- src/WordCount.java
+++ src/WordCount.java
@@ -9,22 +9,32 @@ import java.util.regex.Pattern;
 public class WordCount {
     public static void main(String [] args) {
         int count = 0;
-        String str = null;
-        String word = "man";
+        String word = "the";
         String fileName = "F:/Cloudwick/java_Cloudwick/gutenberg/war&peace.txt";
+        FileReader file = null;
+        BufferedReader br = null;
+        String currentLine = null;
+        String[] arrayList = null;
+
         Pattern p = Pattern.compile("\\b" + word + "\\b", Pattern.CASE_INSENSITIVE);
+
         try {
-            Scanner file = new Scanner(new FileReader(fileName));
-            while(file.hasNext()){
-                str = (file.next());
-                Matcher M = p.matcher(str);
-                if (M.find()){
-                    count = count+1;
+            file = new FileReader(fileName);
+            br = new BufferedReader(file);
+            
+            while ((currentLine = br.readLine()) != null) {
+                arrayList = currentLine.split("[^a-zA-Z]");
+                for(String str : arrayList){
+                    Matcher M = p.matcher(str);
+                    if (M.find()){
+                        count++;
+                    }
                 }
             }
+            System.out.print("Total Count is : ");
             System.out.print(count);
         }
-        catch(FileNotFoundException e){
+        catch(Exception e){
             System.out.println("File not found !!");
         }
 
